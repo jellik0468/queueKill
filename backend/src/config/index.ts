43b0c48d90c52266427be914cb978357d882cf0a
Kenny Wong <wongkenny1379@gmail.com>
2025-12-04 04:20:28 +1100

    fixing render .env fallback

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
-import path from 'path';
 
 // Load environment variables
-dotenv.config({ path: path.join(__dirname, '../../.env') });
-
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 export const config = {
   // Server
   nodeEnv: process.env.NODE_ENV || 'development',
